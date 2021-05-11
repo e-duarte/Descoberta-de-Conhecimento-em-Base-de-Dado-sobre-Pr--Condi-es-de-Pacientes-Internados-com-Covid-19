39feafc14c0c6d4e2f27e30ffb527791223755fb
João Souza <joaosouza@alunos.utfpr.edu.br>
2021-05-11 12:01:05 -0300

    the tables was replace and refactores

--- src/create_tables.py
+++ src/create_tables.py
@@ -3,12 +3,18 @@ import pandas as pd
 dataset = pd.read_csv('./data/preprocessed-covid.csv')
 
 factor_tb = dataset[['state', 'entry_date', 'id']].copy()
-date_dm = dataset[['entry_date', 'date_symptoms', 'date_died']]
+entry_date_dm = dataset['entry_date'].copy().drop_duplicates()
+date_symptoms_dm = dataset['date_symptoms'].copy().drop_duplicates()
+date_died_dm = dataset['date_died'].copy().drop_duplicates()
+
 patient_dm = dataset[['id', 'sex']]
+print(len(patient_dm))
+patient_dm = dataset[['id', 'sex']].drop_duplicates()
+print(len(patient_dm))
+# exit()
 preconditions_dm = dataset[
     list(set(dataset.columns.tolist()) - set(['id', 'sex', 'entry_date', 'date_symptoms', 'date_died', 'age']))
-]
-
+].drop_duplicates()
 
 
 factor_tb['id_preconditions'] = [i for i in range(len(dataset))]
@@ -17,6 +23,10 @@ preconditions_dm['id_preconditions']  = factor_tb['id_preconditions'].copy()
 
 
 factor_tb.to_csv('./tables/factor_tb.csv', index=False)
-date_dm.to_csv('./tables/date_dm.csv', index=False)
+
+entry_date_dm.to_csv('./tables/entry_date_dm.csv', index=False)
+date_symptoms_dm.to_csv('./tables/date_symptoms_dm.csv', index=False)
+date_died_dm.to_csv('./tables/date_died_dm.csv', index=False)
+
 patient_dm.to_csv('./tables/patient_dm.csv', index=False)
 preconditions_dm.to_csv('./tables/preconditions_dm.csv', index=False)
--- src/dataset.py
+++ src/dataset.py
@@ -1,36 +1,68 @@
 from numpy.lib.function_base import cov
 import pandas as pd
+import numpy as np
 
 class CovidDataset:
     def __init__(self):
         covid = pd.read_csv('data/covid.csv')
 
         covid['sex'] = covid['sex'] - 1
+        # covid['sex'] = list(
+        #         map(
+        #             lambda x: 'female' if x == 1 else 'male',
+        #             covid['sex']
+        #         )
+        #     )
         covid['patient_type'] = covid['patient_type'] - 1
+        covid['patient_type'] = list(
+                map(
+                    lambda x: 0 if x == 1 else 1,
+                    covid['patient_type']
+                )
+            )
 
         columns = covid.columns.tolist()
         columns = list(set(columns) - set(['id', 'sex', 'patient_type', 'entry_date', 'date_symptoms', 'date_died', 'age']))
 
 
+        # for c in columns:
+        #     covid[c] = list(
+        #         map(
+        #             lambda x: 'not-specified' if x == 97 or x == 98 or x == 99 else x,
+        #             covid[c]
+        #         )
+        #     )
         for c in columns:
             covid[c] = list(
                 map(
-                    lambda x: 'not-specified' if x == 97 or x == 98 or x == 99 else x,
+                    self.to_map,
                     covid[c]
                 )
             )
-        
-        self.one_hot(covid, columns)
+
+        # self.one_hot(covid, columns)
         
 
         covid['entry_date'] = pd.to_datetime(covid['entry_date'], format='%d-%m-%Y')
         covid['date_symptoms'] = pd.to_datetime(covid['date_symptoms'], format='%d-%m-%Y')
 
         duration_entry_symptoms = covid['entry_date'] - covid['date_symptoms']
-        covid['duration_entry_symptoms'] = duration_entry_symptoms
+        covid['duration_entry_symptoms'] = list(
+            map(
+                lambda x: int(str(x).split(' ')[0]),
+                duration_entry_symptoms
+            )
+        )
     
         covid['duration_hospitalization'] = self.duration_hospitalization(covid)
-        covid['died'] = self.died(covid)
+        # covid['died'] = self.died(covid)
+        covid['died'] = list(
+                map(
+                    lambda x: 0 if x == '9999-99-99' else 1,
+                    covid['date_died']
+                )
+            )
+
 
 
         covid.pop('id')
@@ -38,8 +70,9 @@ class CovidDataset:
         covid.pop('date_symptoms')
         covid.pop('date_died')
 
-        for c in columns:
-            covid.pop(c)
+        # for c in columns:
+        #     covid.pop(c)
+
         print(covid.dtypes)
         self.covid = covid
     
@@ -85,6 +118,13 @@ class CovidDataset:
 
         return died
 
+    def to_map(self, x):
+        if x == 1:
+            return 1
+        elif x == 2:
+            return 0
+        else:
+            return 3
 
 
 class CovidDataWarehouse:
@@ -166,10 +206,43 @@ class CovidDataWarehouse:
         else:
             return 'not-specified'
 
-
-
-
 if __name__ == '__main__':
     from sklearn.cluster import KMeans
-   
-    CovidDataWarehouse().covid.to_csv('data/preocessed-covid.csv', index=False)
+    import matplotlib.pyplot as plt
+
+    covid_datawarehouse = CovidDataWarehouse().covid
+    covid_datawarehouse.pop('id')
+    covid_datawarehouse.pop('age')
+    covid_datawarehouse.pop('sex')
+    covid_datawarehouse.pop('patient_type')
+    covid_datawarehouse.pop('entry_date')
+    covid_datawarehouse.pop('date_symptoms')
+    covid_datawarehouse.pop('date_died')
+    covid_datawarehouse.pop('state')
+
+    covid_datawarehouse = covid_datawarehouse.drop_duplicates()
+
+    print(len(covid_datawarehouse))
+    # covid_datawarehouse.to_csv('data/preprocessed-covid.csv', index=False)
+    # print('Preprocessing dataset')
+    # dataset = CovidDataset().covid
+    # dataset.to_csv('out/covid-dataset-preprocessed.csv', index=False)
+    # exit()
+    # # X = pd.read_csv('out/covid-dataset-preprocessed.csv')
+    
+    # print('Applyng K-means algorithms')
+    # kmeans = KMeans(n_clusters=5, random_state=0).fit(X)
+    # clusters = kmeans.fit_predict(X)
+
+    # plt.figure(figsize = (12,12))
+    # plt.scatter(X['sex'], X['age'], alpha=0.3) 
+    # # plt.xlabel(c1label, fontsize=18)
+    # # plt.ylabel(c2label, fontsize=18)
+    # # plt.suptitle(title, fontsize=20)
+    # # plt.savefig(title + '.jpg')
+    # plt.show()
+
+
+    
+
+
